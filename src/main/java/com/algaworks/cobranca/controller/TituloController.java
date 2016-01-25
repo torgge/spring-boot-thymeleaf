cd498440c533005b1f380bfd45fa1e2b45eb58a4
torgge <torgge@gmail.com>
2016-01-25 15:14:36 -0300

    ModelAndView e Thymeleaf
    Utilização do ModelAndView, como retorno do controller e alteração da
    view. Utilizandos as tags do thymeleaf.

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -1,12 +1,14 @@
 package com.algaworks.cobranca.controller;
 
-        import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.stereotype.Controller;
-        import org.springframework.web.bind.annotation.RequestMapping;
-        import org.springframework.web.bind.annotation.RequestMethod;
+import com.algaworks.cobranca.model.StatusTitulo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-        import com.algaworks.cobranca.model.Titulo;
-        import com.algaworks.cobranca.repository.Titulos;
+import com.algaworks.cobranca.model.Titulo;
+import com.algaworks.cobranca.repository.Titulos;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/titulos")
@@ -16,14 +18,18 @@ public class TituloController {
     private Titulos titulos;
 
     @RequestMapping("/novo")
-    public String novo(){
-        return "CadastroTitulo";
+    public ModelAndView novo() {
+        ModelAndView mv = new ModelAndView("CadastroTitulo");
+        mv.addObject("todosStatusTitulo", StatusTitulo.values());
+        return mv;
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public String salvar(Titulo titulo){
-
+    public ModelAndView salvar(Titulo titulo) {
         titulos.save(titulo);
-        return "CadastroTitulo";
+
+        ModelAndView mv = new ModelAndView("CadastroTitulo");
+        mv.addObject("mensagem", "Título salvo com sucesso!");
+        return mv;
     }
 }
