a2710f1d866ee8e90b3dc15cdb074931860ac888
torgge <torgge@gmail.com>
2016-01-26 09:16:24 -0300

    Alteração do Status
    Alteração do status no thymeleaf.
    E no controller

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -3,12 +3,16 @@ package com.algaworks.cobranca.controller;
 import com.algaworks.cobranca.model.StatusTitulo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.algaworks.cobranca.model.Titulo;
 import com.algaworks.cobranca.repository.Titulos;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/titulos")
@@ -20,7 +24,6 @@ public class TituloController {
     @RequestMapping("/novo")
     public ModelAndView novo() {
         ModelAndView mv = new ModelAndView("CadastroTitulo");
-        mv.addObject("todosStatusTitulo", StatusTitulo.values());
         return mv;
     }
 
@@ -32,4 +35,13 @@ public class TituloController {
         mv.addObject("mensagem", "Título salvo com sucesso!");
         return mv;
     }
+
+    @ModelAttribute("todosStatusTitulo")
+    public List<StatusTitulo> todosStatusTitulo(){
+        return Arrays.asList(StatusTitulo.values());
+    }
 }
+
+
+
+
