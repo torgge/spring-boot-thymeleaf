5421a9609948ec498a7c7788a0bf732eee21d3c0
George Bonespírito <torgge@gmail.com>
2016-02-23 00:34:02 -0300

    Aula 2.15

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -20,12 +21,14 @@ import java.util.List;
 @RequestMapping("/titulos")
 public class TituloController {
 
+    final static private String CADASTRO_VIEW = "CadastroTitulo";
+
     @Autowired
     private Titulos titulos;
 
     @RequestMapping("/novo")
     public ModelAndView novo() {
-        ModelAndView mv = new ModelAndView("CadastroTitulo");
+        ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
         mv.addObject(new Titulo());
         return mv;
     }
@@ -33,7 +36,7 @@ public class TituloController {
     @RequestMapping(method = RequestMethod.POST)
     public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
         if (errors.hasErrors()) {
-            return "CadastroTitulo";
+            return CADASTRO_VIEW;
         }
 
         titulos.save(titulo);
@@ -46,6 +49,13 @@ public class TituloController {
         List<Titulo> todosTitulos = titulos.findAll();
         ModelAndView mv = new ModelAndView("PesquisaTitulos");
         mv.addObject("titulos", todosTitulos);
+        return mv;
+    }
+
+    @RequestMapping("{codigo}")
+    public ModelAndView edicao(@PathVariable("codigo") Titulo titulo) {
+        ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+        mv.addObject(titulo);
         return mv;
     }
 
